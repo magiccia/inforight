f2c298e0f12a27270a345470a1db1bba7e846bdf
magiccia <magiccia@gmail.com>
2015-08-17 22:21:39 -0400

    finish search box
    in html, css and js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
+var $quicksearch;
+var qsRegex;
 var $grid;
 
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
+
 $(document).ready(function(){
-  $grid = $(".grid").isotope({ "itemSelector": ".country-box", "layoutMode": "fitRows" });
+  $grid = $(".grid").isotope({ 
+    itemSelector: ".country-box", 
+    layoutMode: "fitRows",
+    filter: function() {
+      return qsRegex ? $(this).text().match( qsRegex ) : true;
+    } 
+  });
+
+  // use value of search field to filter
+  $quicksearch = $('.quicksearch').keyup( debounce( function() {
+    qsRegex = new RegExp( $quicksearch.val(), 'gi' );
+    $grid.isotope();
+  }, 200 ) );
+
 });
